232d13a3d8eef2c69399d820237e39ebe4076e61
Ayaan Shaikh <ayaannshaikhh8@gmail.com>
2025-11-21 13:58:52 -0500

    fixed supabase file path

--- frontend/pages/main.js
+++ frontend/pages/main.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from '../../supabaseClient'
+import { supabase } from '../supabaseClient'
 
 export default function Main() {
   const [resume, setResume] = useState(null)
